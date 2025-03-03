30dd153a897023b638365f8240e229ad75e3131f
Andres Almanzar Restrepo <aalmanzar@rentingcolombia.com>
2025-03-02 23:59:09 -0500

    remoteCa/remoteHn removed, update packages and set Tests

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,15 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone-testing';
+import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
+declare const require: any;
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
+// Busca y carga automáticamente todos los archivos `*.spec.ts`
+const context = require.context('./', true, /\.spec\.ts$/);
+context.keys().map(context);
